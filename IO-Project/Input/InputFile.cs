ae3f44eb86fac1fc1ec21ca0a23db07d54c15a57
Konrad Broda <konrad@weastronauts.pl>
2020-01-21 21:42:25 +0100

    Add file open and display properties tool

---
+++ IO-Project/Input/InputFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO_Project.Input
+{
+    class InputFile
+    {
+        public string filename;
+        public string path;
+        public string content;
+        public long size;
+
+        public override string ToString()
+        {
+            return $"Filename: {filename} \n" +
+                   $"Full path: {path} \n" +
+                   $"Size: {size} \n";
+        }
+    }
+}
--- IO-Project/Input/InputTestForm.cs
+++ IO-Project/Input/InputTestForm.cs
@@ -3,18 +3,57 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Console = System.Console;
 
 namespace IO_Project.Input
 {
     public partial class InputTestForm : Form
     {
+        string pathSource = @"C:\dev\mesfem\mesfem\FEM\UniversalElement.cs";
+
+        List<InputFile> inputFiles = new List<InputFile>();
+
         public InputTestForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Select a valid C# class file";
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+
+            var newInputFile = new InputFile();
+            newInputFile.filename = Path.GetFileName(openFileDialog1.FileName);
+            newInputFile.path = openFileDialog1.FileName;
+            newInputFile.content = File.ReadAllText(openFileDialog1.FileName);
+            newInputFile.size = 0;
+            inputFiles.Add(newInputFile);
+
+            listBox1.Items.Add(newInputFile.path);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = inputFiles[listBox1.SelectedIndex].content;
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0) // unselected = -1
+            {
+                MessageBox.Show(inputFiles[listBox1.SelectedIndex].ToString(), "File Info");
+            }
         }
     }
 }
