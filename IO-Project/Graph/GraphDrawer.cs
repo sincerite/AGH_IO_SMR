eb894f222a6dd7ec3e4c849b1f17617cf5b77039
Dominika Duda <duda.dominika97@gmail.com>
2020-01-22 17:42:10 +0100

    [Graph] Add GraphDrawer

---
+++ IO-Project/Graph/GraphDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IO_Project.Core.Analysis.Models;
+using Microsoft.Msagl.GraphViewerGdi;
+
+namespace IO_Project.Graph
+{
+    class GraphDrawer
+    {
+        private GViewer _gViewer;
+
+        public GraphDrawer(GViewer gViewer)
+        {
+            _gViewer = gViewer;
+        }
+
+        public void GenerateGraphForSourceAnalysis(SourceAnalysisModel model)
+        { 
+        
+        }
+
+    }
+}
